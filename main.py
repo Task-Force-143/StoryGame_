3ef12fb77ed3f2afbbbc67c78a5c1e030dffbb06
sphnix <120174825+Sphnixken@users.noreply.github.com>
2024-01-13 12:29:52 +0530

    Template for story game

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-import random
+#----modules---------#
+import os
+import datetime
+#----------------------
 
-def choose_door():
-    print("Choose a door: 1 or 2")
-    choice = input()
-    return int(choice)
+#-----login-----sphnix#
 
-def play_game():
-    correct_door = random.randint(1, 2)
-    
-    print("Welcome to the Two-Door Game!")
-    print("Behind one door you will pass, and behind the other door you will fail.")
-    
-    user_choice = choose_door()
-    
-    if user_choice == correct_door:
-        print("Congratulations! You chose the correct door. You win!")
-    else:
-        print(f"Sorry, you chose the wrong door. The correct door was {correct_door}. Better luck next time!")
 
-if __name__ == "__main__":
-    play_game()
-   for w in "" 
-for m in range(15):
-    print(m)
+
+
+
+#---------------------#
+
+
+
+
+#-----story part-----#
+
+
+#--------------------#
