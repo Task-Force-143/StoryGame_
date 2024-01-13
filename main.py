53810bcf9f035d09e1be3b095a8d184844122309
sphnix <120174825+Sphnixken@users.noreply.github.com>
2024-01-13 20:30:50 +0530

    Login part(sid)

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #----modules---------#
 import os
 import datetime
+import time
 #----------------------
 
 #-----login-----sphnix#
 
 
-
+print("Wlcome to this program")
+time.sleep(1)
+print("wait not acctually a program a game ")
+time.sleep(2)
 
 
 #---------------------#
