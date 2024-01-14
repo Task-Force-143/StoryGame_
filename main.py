22672c801e924f3344a8843531b1078fc3269bcf
sphnix <120174825+Sphnixken@users.noreply.github.com>
2024-01-14 09:48:18 +0530

    Login screen stage 3 (SID)

--- main.py
+++ main.py
@@ -7,13 +7,27 @@ import time
 #-----login-----sphnix#
 
 
+registered_users = {"user1" : "password1", "user2": "password2"}
+
 print("Welcome to this program")
 time.sleep(1)
 print("wait not actually a program a game ")
 time.sleep(2)
+
 loin_screen = input("Login\nSign Up\nCredit\n")
+
 if loin_screen == "Login":
-    #SID
+    login_us = input("Username: ")
+    login_pas = input("Password: ")
+
+    if login_us in registered_users and registered_users[login_us]:
+        print("Login Successful")
+
+    else:
+        print("Wrong Password")
+elif loin_screen == "Sign Up":
+    print("SID")
+
 
 #---------------------#
 
